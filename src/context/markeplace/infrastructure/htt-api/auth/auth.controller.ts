d576148ef8f1b116d2894e6d1b041fbcb29c0763
Fabio Andres Arango Grajales <fabio.andres.arango72@gmail.com>
2024-08-15 09:54:40 -0500

    feat: swagger configuration

--- src/context/markeplace/infrastructure/htt-api/auth/auth.controller.ts
+++ src/context/markeplace/infrastructure/htt-api/auth/auth.controller.ts
@@ -1,11 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthUseCase } from '../../../application/auth-use-case/auth-use-case';
+import {
+  ApiOperation,
+  ApiProduces,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('Auth')
 export class AuthController {
   constructor(private authUseCase: AuthUseCase) {}
 
-  @Get()
+  @Post('authentication')
+  @ApiProduces('application/json')
+  @ApiOperation({
+    summary: 'user authentication',
+  })
+  @ApiProduces('application/json')
+  @ApiResponse({
+    status: 200,
+    description: 'user authentication',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Error: Unauthorized.',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error.',
+  })
   async run() {
     console.log('***********INFRASTRUCTURE****************');
     return this.authUseCase.execute();
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/marketplace');
+  app.enableCors();
+  const swagger = new DocumentBuilder()
+    .setTitle('API MarketPlace')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swagger);
+  SwaggerModule.setup('api/swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
